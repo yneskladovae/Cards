a989c82f21a734e412ae04791d3ef9ec346e92e2
Stanislav Moskalenko <maskal2719@gmail.com>
2023-05-29 15:17:27 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logologologologologologologo"
+        />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
