7851f1ae329cb1213a0d3b0176fd2ac7cc4d2669
yneskladovae <yneskladovae27@gmail.com>
2023-05-31 21:23:51 +0300

    feat: add forgot thunk, addCase in slice for forgot, styles forgot and check email forms

--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -9,6 +9,18 @@ import {
 } from "features/auth/auth.api";
 import { createAppAsyncThunk } from "common/utils/create-app-async-thunk";
 
+const forgot = createAsyncThunk(
+  "auth/forgot",
+  async (arg: ArgsForgotType, thunkAPI) => {
+    try {
+      const res = await authApi.forgot(arg);
+      return res.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
 const register = createAsyncThunk(
   "auth/register",
   async (arg: ArgRegisterType, thunkAPI) => {
@@ -33,13 +45,6 @@ const login = createAppAsyncThunk<{ profile: ProfileType }, ArgLoginType>(
   }
 );
 
-const forgot = createAppAsyncThunk(
-  "auth/forgot",
-  async (args: ArgsForgotType) => {
-    const res = await authApi.forgot(args);
-  }
-);
-
 const setNewPassword = createAppAsyncThunk(
   "auth/set-new-password",
   async (args: ArgsSetNewPasswordType) => {
@@ -54,6 +59,7 @@ const slice = createSlice({
     isLoading: false,
     error: "",
     isReg: false,
+    isForgot: false,
   },
   reducers: {},
   extraReducers: (builder) => {
@@ -82,10 +88,28 @@ const slice = createSlice({
         state.isLoading = false;
         state.isReg = false;
         state.error = "Some error";
+      })
+
+      .addCase(forgot.fulfilled, (state, action) => {
+        state.isForgot = true;
+        state.isLoading = false;
+      })
+      .addCase(forgot.pending, (state, action) => {
+        state.isForgot = false;
+        state.isLoading = true;
+      })
+      .addCase(forgot.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isForgot = false;
       });
   },
 });
 
 export const authReducer = slice.reducer;
 export const authSlice = slice.actions;
-export const authThunks = { register, login, forgot, setNewPassword };
+export const authThunks = {
+  register,
+  login,
+  forgot,
+  setNewPassword,
+};
